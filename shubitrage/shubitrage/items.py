12a42a4f4bb2ed7aa671e6c3af8b36198513ebd5
obafemi-i <obaff76@gmail.com>
2023-03-19 16:25:36 +0100

    updates

---
+++ shubitrage/shubitrage/items.py
@@ -0,0 +1,19 @@
+# Define here the models for your scraped items
+#
+# See documentation in:
+# https://docs.scrapy.org/en/latest/topics/items.html
+
+import scrapy
+
+
+class ShubitrageItem(scrapy.Item):
+    # define the fields for your item here like:
+    sku=scrapy.Field()
+    Nike_name = scrapy.Field()
+    Nike_category = scrapy.Field()
+    Nike_selling_price = scrapy.Field()
+    Nike_original_price = scrapy.Field()
+    Nike_status = scrapy.Field()
+    stockx_price=scrapy.Field()
+    stockx_status=scrapy.Field()
+    stockx_name = scrapy.Field()
---
+++ shubitrage/shubitrage/spiders/Nike.py
@@ -0,0 +1,52 @@
+import scrapy
+
+from ..items import ShubitrageItem
+class NikeSpider(scrapy.Spider):
+    name = "Nike"
+    allowed_domains = ["Nike.com", "stockx.com"]
+
+    sku = input('Enter SKU - ')
+
+    start_urls = [f"https://www.nike.com/w?q={sku}"]
+
+    def parse(self, response):
+        shoe_name = response.css('div.product-card__title::text').get()
+        shoe_category = response.css('div.product-card__subtitle::text').get()
+
+        selling_price = response.css('div.is--current-price::text').get()
+
+        try:
+            original_price = response.css('div.is--striked-out::text').get()
+        except AttributeError:
+            original_price = None
+
+        try:
+            best_seller = response.css('div.product-card__messaging.accent--color::text').get()
+        except AttributeError:
+            best_seller = None
+
+        items = ShubitrageItem()
+        
+        items['sku'] = NikeSpider.sku
+        items['Nike_category'] = shoe_category
+        items['Nike_name'] = shoe_name
+        items['Nike_selling_price'] = selling_price
+        items['Nike_original_price'] = original_price
+        items['Nike_status'] = best_seller
+
+        stockx_url = f'https://stockx.com/search?s={NikeSpider.sku}'
+
+        yield scrapy.Request(stockx_url, callback=self.parse_stockx, cb_kwargs={'items':items})
+
+    def parse_stockx(self, response, items):
+        main = response.css('div.css-xzkzsa')
+        for search in main:
+            stockx_name = search.css('p.chakra-text.css-3lpefb::text').extract()
+            stockx_status = search.css('p.chakra-text.css-jsq4f::text').extract()
+            stockx_price = search.css('p.chakra-text.css-nsvdd9::text').extract()
+
+            items['stockx_price'] = stockx_price
+            items['stockx_name'] = stockx_name
+            items['stockx_status'] = stockx_status
+        
+        yield items
