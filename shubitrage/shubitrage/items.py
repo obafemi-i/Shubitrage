42a2f1c54bf6b0e6dff35698618b3ce5470d6e3e
Obafemi <59392583+obafemi-i@users.noreply.github.com>
2023-12-07 23:16:38 +0100

    Update items.py

--- shubitrage/shubitrage/items.py
+++ shubitrage/shubitrage/items.py
@@ -5,15 +5,14 @@
 
 import scrapy
 
-
 class ShubitrageItem(scrapy.Item):
-    # define the fields for your item here like:
-    sku=scrapy.Field()
+    sku = scrapy.Field()
     Nike_name = scrapy.Field()
     Nike_category = scrapy.Field()
     Nike_selling_price = scrapy.Field()
     Nike_original_price = scrapy.Field()
     Nike_status = scrapy.Field()
-    stockx_price=scrapy.Field()
-    stockx_status=scrapy.Field()
+    stockx_price = scrapy.Field()
+    stockx_status = scrapy.Field()
     stockx_name = scrapy.Field()
+    
